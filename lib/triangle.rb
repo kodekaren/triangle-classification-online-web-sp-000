1f5e40643dec18afc359f166d907999b63166181
karen ercoli <karenercoli@gmail.com>
2019-01-27 16:41:38 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,9 +8,9 @@ def initialize(length_one, length_two, length_three)
 end
 
 def kind
-  if length_one == 0 || length_two == 0 || length_three == 0
+  if length_one <= 0 || length_two <= 0 || length_three <= 0
     raise TriangleError
-
+  
   elsif length_one == length_two && length_two == length_three
     :equilateral
   elsif
