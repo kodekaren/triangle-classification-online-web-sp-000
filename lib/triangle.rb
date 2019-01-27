a1473ede2153dab7cdcb879b5644e885175db98b
karen ercoli <karenercoli@gmail.com>
2019-01-27 16:25:00 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,13 +2,13 @@ class Triangle
 attr_accessor :length_one, :length_two, :length_three
 
 def initialize(length_one, length_two, length_three)
-  @length_one > 0
-  @length_two > 0
-  @length_three > 0
+  @length_one = length_one
+  @length_two = length_two
+  @length_three = length_three
 end
 
 def kind
-  if length_one>0 && == length_two && length_two == length_three
+  if length_one > 0 && == length_two && length_two == length_three
     :equilateral
   elsif
     length_one == length_two || length_two == length_three || length_one == length_three
