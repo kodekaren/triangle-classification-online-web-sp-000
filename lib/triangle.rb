985eb30b07cd98906b601b66054fc2c474c9a87e
karen ercoli <karenercoli@gmail.com>
2019-01-27 16:45:22 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,6 +11,7 @@ def kind
   if length_one <= 0 || length_two <= 0 || length_three <= 0
     raise TriangleError
   elsif length_one + length_two > length_three
+    raise TriangleError
 
   elsif length_one == length_two && length_two == length_three
     :equilateral
