e364293d37f2067757488e6c66b90d923fb0ecda
karen ercoli <karenercoli@gmail.com>
2019-01-27 16:38:16 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,10 @@ def initialize(length_one, length_two, length_three)
 end
 
 def kind
-  if length_one == length_two && length_two == length_three
+  if length_one == 0 || length_two == 0 || length_three == 0
+    raise TriangleError
+    
+  elsif length_one == length_two && length_two == length_three
     :equilateral
   elsif
     length_one == length_two || length_two == length_three || length_one == length_three
@@ -17,7 +20,7 @@ def kind
     length_one != length_two && length_two != length_three
     :scalene
   else
-    raise TriangleError
+     raise TriangleError
   end
 end
 class TriangleError < StandardError
